8f5e2b2cd2738ffb44c21b229a112e1ff80874dd
Nick Hengeveld <nickh@hengeveld.com>
2009-11-13 22:38:00 -0800

    Added model rdoc

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
+# The Question model provides an interface to individual survey questions.
+# Each question must have a blurb (the text of the question.)
 class Question < ActiveRecord::Base
   validates_presence_of :blurb
 end
--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,3 +1,6 @@
+# The Responder model provides an interface to a "party" that is answering
+# survey questions.  Responders must have unique email addresses and may
+# have an associated name.
 class Responder < ActiveRecord::Base
   validates_format_of :email_address, :with => /^[^\@]+\@[^\@]+\.[^\@]+$/
 end
