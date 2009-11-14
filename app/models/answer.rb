0b945b4c9b9180188e57446035c8b3edef832cc2
Nick Hengeveld <nickh@hengeveld.com>
2009-11-13 23:26:04 -0800

    Made answer tests pass

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 # The Responder model provides an interface to survey answers.  Each answer
 # belongs to a question and a responder.
 class Answer < ActiveRecord::Base
+  belongs_to :question
+  belongs_to :responder
 end
