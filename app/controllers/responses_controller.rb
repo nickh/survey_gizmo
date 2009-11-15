812900af350a6ad8b6097bc34d8d80188cc99018
Nick Hengeveld <nickh@hengeveld.com>
2009-11-15 09:15:33 -0800

    Added responses controller functionality and tests
    #index, #new, #create, #show

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,16 +1,51 @@
 class ResponsesController < ApplicationController
+  before_filter :set_response
+
   def index
+    redirect_to @response.nil?? new_response_path : response_path(@response)
   end
 
   def new
+    email_address = session[:respondent].email_address rescue nil
+    name          = session[:respondent].name          rescue nil
+    @respondent   = Respondent.new(:email_address => email_address, :name => name)
   end
 
   def create
+    respondent_params = params[:respondent] || {}
+    email_address    = respondent_params[:email_address]
+    if email_address.blank?
+      return redirect_to(new_response_path)
+    elsif existing_respondent = Respondent.find_by_email_address(email_address)
+      session[:respondent] = existing_respondent
+      @response = existing_respondent.response
+    else
+      new_respondent = Respondent.create(respondent_params)
+      session[:respondent] = new_respondent
+      return redirect_to(new_response_path) if new_respondent.new_record?
+      @response = new_respondent.response
+    end
+
+    redirect_to(response_path(@response))
   end
 
   def show
+    redirect_to(new_response_path) if @response.nil? || params[:id].to_i != @response.id
   end
 
   def update
+  end
+
+  private
+
+  def set_response
+    if session[:respondent]
+      begin
+        session[:respondent].reload
+        @response = session[:respondent].response
+      rescue
+        nil
+      end
+    end
   end
 end
--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -1,82 +1,126 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ResponsesController do
-  describe "when (re)starting a response session" do
-    before do
-      @test_addr = 'foo@bar.org'
-      @test_name = 'Foo Bar'
+  before do
+    @test_addr = 'foo@bar.org'
+    @test_name = 'Foo Bar'
+  end
+
+  describe '#index' do
+    describe 'when there is a valid respondent in the session' do
+      it 'should redirect to show' do
+        valid_respondent = Respondent.create(:email_address => @test_addr, :name => @test_name)
+        session[:respondent] = valid_respondent
+        get :index
+        response.should redirect_to(response_path(valid_respondent))
+      end
     end
 
-    it "requires a valid responder to get started" do
-      session[:responder] = nil
-      get :index
+    describe 'when there is an invalid respondent in the session' do
+      it 'should redirect to new' do
+        invalid_respondent = Respondent.create(:email_address => @test_addr, :name => @test_name)
+        Respondent.delete(invalid_respondent.id)
+        session[:respondent] = invalid_respondent
+        get :index
+        response.should redirect_to(new_response_path)
+      end
+    end
+
+    describe 'when there is no respondent in the session' do
+      it 'should redirect to new' do
+        session[:respondent] = nil
+        get :index
+        response.should redirect_to(new_response_path)
+      end
+    end
+  end
+
+  # Be nice to returning visitors, if there's still a Respondent object in the session,
+  # use it to prepopulate the email address and name in the form
+  describe '#new' do
+    it 'should use the email address and name from the respondent in the session if it exists' do
+      session[:respondent] = Respondent.create(:email_address => @test_addr, :name => @test_name)
+      get :new
+      assigns[:respondent].email_address.should == @test_addr
+      assigns[:respondent].name.should          == @test_name
+      response.should render_template('new')
+    end
+
+    it 'should have no email address or name if no respondent exists in the session' do
+      session.delete(:respondent)
+      get :new
+      assigns[:respondent].email_address.should be_nil
+      assigns[:respondent].name.should          be_nil
+      response.should render_template('new')
+    end
+  end
+
+  # Handle a POST for a new response session - if a respondent with the specified
+  # email address already exists, restart the existing session; otherwise
+  # create a new one.
+  describe '#create' do
+    describe 'when no email address is specified' do
+      it 'should redirect to new' do
+        post :create
+        response.should redirect_to(new_response_path)
+      end
+    end
+
+    describe 'when an existing email address is specified' do
+      it 'should redirect to the existing response show page' do
+        existing_respondent = Respondent.create(:email_address => @test_addr, :name => @test_name)
+        existing_response  = existing_respondent.response
+        existing_response.should_not be_nil
+        post :create, :respondent => {:email_address => @test_addr}
+        response.should redirect_to(response_path(existing_response))
+      end
+    end
+
+    describe 'when a valid new email_address is specified' do
+      it 'should redirect to a new response show page' do
+        Respondent.find_by_email_address(@test_addr).should be_nil
+        post :create, :respondent => {:email_address => @test_addr}
+        new_respondent = Respondent.find_by_email_address(@test_addr)
+        new_respondent.should_not be_nil
+        new_response  = new_respondent.response
+        response.should redirect_to(response_path(new_response))
+      end
+    end
+
+    describe 'when an invalid new email_address is specified' do
+      it 'should redirect to new' do
+        mock_respondent = mock(Respondent.new)
+        mock_respondent.should_receive(:new_record?).and_return true
+        Respondent.stub!(:create).and_return(mock_respondent)
+        post :create, :respondent => {:email_address => 'something_invalid'}
+        response.should redirect_to(new_response_path)
+      end
+    end
+  end
+
+  describe '#show' do
+    it 'should redirect to new if there is no respondent in the session' do
+      test_response = Response.create
+      session[:respondent] = nil
+      get :show, :id => test_response.id
       response.should redirect_to(new_response_path)
     end
 
-    it "populates the responder email address and name from the session" do
-      session[:email_address] = @test_addr
-      session[:name]          = @test_name
-      get :new
-      assigns[:email_address].should == @test_addr
-      assigns[:name].should          == @test_name
+    it 'should redirect to new if the respondent and response do not match' do
+      test_respondent = Respondent.create(:email_address => @test_addr, :name => @test_name)
+      test_response   = test_respondent.response
+      other_response  = Response.create
+      session[:respondent] = test_respondent
+      get :show, :id => other_response.id
+      response.should redirect_to(new_response_path)
     end
 
-    it "restarts a response if a valid responder is present" do
-      test_responder = Responder.create(:email_address => @test_addr, :name => @test_name)
-      test_response  = Response.create(:responder => test_responder)
-      session[:responder] = test_responder
-      get :index
-      response.should redirect_to(response_path(test_response))
-    end
-
-    it "creates a new responder" do
-      Responder.find_by_email_address(@test_addr).should be_nil
-      post :create, :responder => {:email_address => @test_addr, :name => @test_name}
-      new_responder = Responder.find_by_email_address(@test_addr)
-      new_responder.should_not be_nil
-      new_responder.response.should_not be_nil
-      assigns[:response].should == new_response
-    end
-
-    it "uses an existing responder" do
-      existing_responder = Responder.create(:email_address => @test_addr, :name => @test_name)
-      post :create, :responder => {:email_address => @test_addr, :name => @test_name}
-      assigns[:response].should == existing_responder.response
-    end
-
-    it "stores the responder in the session" do
-      session[:responder] = nil
-      post :create, :responder => {:email_address => @test_addr, :name => @test_name}
-      session[:responder].email_address.should == @test_addr
-      session[:responder].name.should          == @test_name
-    end
-
-    it "redirects to the show page" do
-      post :create, :responder => {:email_address => @test_addr, :name => @test_name}
-      response.should redirect_to('/show')
-    end
-  end
-
-  describe "when showing questions" do
-    it "shows the next unanswered question" do
-      responder = Responder.create(:email_address => @test_addr, :name => @test_name)
-      response  = Response.create(:responder => responder)
-      test_questions = (1..5).collect{|i| Question.create(:id => i, :blurb => "Question #{i}")}
-      test_answers   = (1..3).collect{|i| Answer.create(:id => i, :response => response, :question_id => i)}
-      get :show, :response => response.id
-      assigns[:next_question].should == Question.find(4)
+    it 'should show the response' do
+      test_respondent = Respondent.create(:email_address => @test_addr, :name => @test_name)
+      test_response   = test_respondent.response
+      session[:respondent] = test_respondent
+      get :show, :id => test_response.id
       response.should render_template('show')
     end
-  end
-
-  describe "when submitting answers" do
-    it "adds new answers"
-    it "updates existing answers"
-    it "redirects to the show page"
-  end
-
-  # Finishing an answer session
-  describe "when finishing a session" do
-    it "shows the thank you page after all questions have been answered"
   end
 end
