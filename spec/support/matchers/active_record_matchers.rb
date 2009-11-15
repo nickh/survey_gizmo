251c1ceeecbe0a93a44f7a4e7905bc338cf731ed
Nick Hengeveld <nickh@hengeveld.com>
2009-11-14 22:52:02 -0800

    Added more AR association matchers, dried up a bit

--- spec/support/matchers/active_record_matchers.rb
+++ spec/support/matchers/active_record_matchers.rb
@@ -1,16 +1,22 @@
+def check_reflection(object, reflection, macro)
+  klass = object.is_a?(Class) ? object : object.class
+  klass.reflections.has_key?(reflection) && klass.reflections[reflection].macro == macro
+end
+
 Spec::Matchers.define :belong_to do |expected|
-  assoc_class = expected.to_s.camelize.constantize
-  assoc_builder = "build_#{expected}".to_sym
+  match                          {|actual| check_reflection(actual, expected, :belongs_to)}
+  failure_message_for_should     {|actual| "expected #{actual.inspect} to belong_to #{expected.inspect}"}
+  failure_message_for_should_not {|actual| "expected #{actual.inspect} to not belong_to #{expected.inspect}"}
+end
 
-  match do |actual|
-    actual.respond_to?(expected) && actual.send(assoc_builder).is_a?(assoc_class)
-  end
+Spec::Matchers.define :have_one do |expected|
+  match                          {|actual| check_reflection(actual, expected, :has_one)}
+  failure_message_for_should     {|actual| "expected #{actual.inspect} to have_one #{expected.inspect}"}
+  failure_message_for_should_not {|actual| "expected #{actual.inspect} to not have_one #{expected.inspect}"}
+end
 
-  failure_message_for_should do |actual|
-    "expected #{actual.inspect} to belong_to #{expected.inspect}"
-  end
-
-  failure_message_for_should_not do |actual|
-    "expected #{actual.inspect} to not belong_to #{expected.inspect}"
-  end
+Spec::Matchers.define :have_many do |expected|
+  match                          {|actual| check_reflection(actual, expected, :has_many)}
+  failure_message_for_should     {|actual| "expected #{actual.inspect} to have_many #{expected.inspect}"}
+  failure_message_for_should_not {|actual| "expected #{actual.inspect} to not have_many #{expected.inspect}"}
 end
