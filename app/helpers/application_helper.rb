33aadb21f1067bc7cd7110c0b4bf7d947ad31488
Nick Hengeveld <nickh@hengeveld.com>
2009-11-18 10:48:52 -0800

    Added some style, view cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def responder(response)
+    h(response.name + ' <' + response.email_address + '>')
+  end
 end
